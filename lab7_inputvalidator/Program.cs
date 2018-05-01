74cd9c604105bcb279dc6bcdc040218a83a0dcb5
saunlani <saunlani@gmail.com>
2018-05-01 00:33:06 -0400

    added a menu for the various checkers, + HTML checker

--- lab7_inputvalidator/Program.cs
+++ lab7_inputvalidator/Program.cs
@@ -19,23 +19,84 @@ namespace lab7_inputvalidator
          * 
          * Method that will validate date based on the format of dd/mm/yyyy.
          * 
-         * use regexr.com
-         * 
-         * ex challenges: method for validating html elements.  (<p> and </p>
+         * TODO: ex challenges: method for validating html elements.  (<p> and </p>
          * are valid element formats.  <h1 <h1> are not,  dont worry about 
-         * special cases.
-         * 
-         * God speed. */
+         * special cases. */
 
         static void Main(string[] args)
         {
-            NameChecker();
-            EmailChecker();
-            PhoneNumberChecker();
-            DateChecker();
-
+            Chooser();
         }
 
+        // menu for the various checkers.
+        static void Chooser()
+        {
+            bool keepgoing = true;
+            while (keepgoing == true)
+            {
+                Console.WriteLine("1.) NameChecker");
+                Console.WriteLine("2.) EmailChecker");
+                Console.WriteLine("3.) PhoneNumberChecker");
+                Console.WriteLine("4.) DateChecker");
+                Console.WriteLine("5.) EmailChecker");
+                Console.WriteLine("6.) HtmlChecker");
+                Console.WriteLine("Enter anything else to quit.");
+
+
+                string userchoice = Console.ReadLine();
+
+                if (userchoice == "1")
+                {
+                    do
+                    {
+                        NameChecker();
+                    } while (AskToInputAgain());
+                }
+
+                else if (userchoice == "2")
+                {
+                    do
+                    {
+                        EmailChecker();
+                    } while (AskToInputAgain());
+                }
+
+                else if (userchoice == "3")
+                {
+                    do
+                    {
+                        PhoneNumberChecker();
+                    } while (AskToInputAgain());
+                }
+
+                else if (userchoice == "4")
+                {
+                    do
+                    {
+                        DateChecker();
+                    } while (AskToInputAgain());
+                }
+
+                else if (userchoice == "5")
+                {
+                    do
+                    {
+                        EmailChecker();
+                    } while (AskToInputAgain());
+                }
+                else if (userchoice == "6")
+                {
+                    do
+                    {
+                        HtmlChecker();
+                    } while (AskToInputAgain());
+                }
+                else
+                {
+                    keepgoing = false;
+                }
+            }
+        }
 
         /* checks for valid name */
         static bool NameChecker()
@@ -74,13 +135,14 @@ namespace lab7_inputvalidator
             }
         }
 
+        //checks for valid email.
         static bool EmailChecker()
         {
             Console.WriteLine("Please enter a valid email in the following format:  example@example.com ");
             while (true)
             {
                 string useremail = Console.ReadLine();
-                if (String.IsNullOrWhiteSpace(useremail) == true 
+                if (String.IsNullOrWhiteSpace(useremail) == true
                     || useremail.Length < 14)
                 {
                     Console.WriteLine("Sorry, that email is invalid!");
@@ -88,7 +150,7 @@ namespace lab7_inputvalidator
                     continue;
                 }
                 else
-                {}
+                { }
 
                 string username = useremail.Split('@')[0];
                 string dompreformat = useremail.Split('@')[1];
@@ -115,6 +177,7 @@ namespace lab7_inputvalidator
             }
         }
 
+        //checks for valid phone number.
         static bool PhoneNumberChecker()
         {
             Console.WriteLine("Please enter a valid phone number in the following format:  555-555-5555");
@@ -128,13 +191,13 @@ namespace lab7_inputvalidator
                     Console.WriteLine("Please enter a valid phone number:  ");
                     continue;
                 }
-                else{}
+                else { }
 
                 string phonep0 = usernumber.Split("-")[0];
                 string phonep1 = usernumber.Split("-")[1];
                 string phonep2 = usernumber.Split("-")[2];
 
-                if (phonep0.Length == 3 
+                if (phonep0.Length == 3
                     && phonep1.Length == 3
                     && phonep2.Length == 4
                     && Regex.IsMatch(phonep0, "^[0-9]+$")
@@ -148,12 +211,13 @@ namespace lab7_inputvalidator
                 {
                     Console.WriteLine("Sorry, that phone number is invalid!");
                     Console.WriteLine("Please enter a valid phone number:  ");
-                    continue;  
+                    continue;
                 }
 
             }
         }
 
+        //checks for valid date.
         static bool DateChecker()
         {
             Console.WriteLine("Please enter a valid date in the following format:  mm/mm/mmmm");
@@ -181,17 +245,125 @@ namespace lab7_inputvalidator
                     && Regex.IsMatch(date2, "^[0-9]+$"))
                 {
                     Console.WriteLine("Valid Date Accepted.");
-                    return true;   
+                    return true;
                 }
                 else
                 {
                     Console.WriteLine("Sorry, that date is invalid!");
                     Console.WriteLine("Please enter a valid date:  ");
-                    continue;  
+                    continue;
                 }
 
- 
+
             }
+        }
+
+        //checks for valid html.
+        static bool HtmlChecker()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter a valid HTML element which follows the format of <ex> or </ex>:  ");
+                string userhtml = Console.ReadLine();
+                string[] indichunks = userhtml.Split(' ');
+                int validinputs = 0;
+                int numchunks = indichunks.Length;
+                /* checks the length of each chunk to verify it's at least 
+              * 3 charcters */
+                if (numchunks != 2 
+                    || indichunks[0].Length < 3
+                    || indichunks[1].Length < 3)
+                {
+                    continue;
+                }
+                else
+                {
+                    numchunks += 1; 
+                    // matches content of the start and end tags
+                    string trimmed1 = indichunks[0].TrimStart('<').TrimEnd('>');
+                    string trimmed2 = indichunks[1].TrimStart('<').TrimStart('/').TrimEnd('>');
+
+                    bool Matcher = true;
+                    if (trimmed1 == trimmed2)
+                    {
+                        Matcher = true;
+                    }
+                    else
+                    {
+                        Matcher = false;
+                    }
+
+                    /* collects info on first and last place of the chunk */
+                    string firstplaceofinput = indichunks[0].Substring(0, 1);
+                    string lastplaceofinput = indichunks[0].Substring(indichunks[0].Length - 1);
+                    string secondplaceinput = indichunks[0].Substring(1, 1);
+
+                    if (firstplaceofinput != "<" || lastplaceofinput != ">" || Matcher == false)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        validinputs += 1;
+                        string firstplaceofinput2 = indichunks[1].Substring(0, 1);
+                        string lastplaceofinput2 = indichunks[1].Substring(indichunks[1].Length - 1);
+                        string secondplaceinput2 = indichunks[1].Substring(1, 1);
+
+                        if (firstplaceofinput2 != "<" || lastplaceofinput2 != ">"
+                            || secondplaceinput2 !="/")
+
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            validinputs += 1;
+                        }
+                            if (validinputs == 2)
+                        {
+                            Console.WriteLine("Valid elements accepted.");
+                            return false;
+                        }
+                        else
+                        {
+                        }
+                    }
+
+
+                }
+            }
+
+        }
+
+        //asks for another input from user.
+        static bool AskToInputAgain()
+        {
+            do
+            {
+                Console.Write("\nEnter another input? (y/n):  ");
+                string userresponse = Console.ReadLine().ToLower();
+                Console.WriteLine("");
+
+
+                if (userresponse == "y")
+                {
+                    return true;
+                }
+                else if (userresponse == "n")
+                {
+                    return false;
+                }
+                else if (userresponse != "y" && userresponse != "n")
+                {
+                    Console.Write("You did not enter a valid response.\n");
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            } while (true);
         }
     }
 }
+
